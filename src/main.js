346a093e332e175ccea9c70666354f1cf921a34d
Jo Jolivet <joj@avi.com>
2025-05-04 23:57:43 -0400

    Add auto create

--- src/main.js
+++ src/main.js
@@ -26,6 +26,16 @@ module.exports.loop = function () {
         }
     }
 
+    var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
+    console.log('Harvesters: ' + harvesters.length);
+
+    if(harvesters.length < 2) {
+        var newName = 'Harvester' + Game.time;
+        console.log('Spawning new harvester: ' + newName);
+        Game.spawns['Spawn1'].spawnCreep([WORK,CARRY,MOVE], newName, 
+            {memory: {role: 'harvester'}});
+    }
+
     for(var name in Game.creeps) {
         var creep = Game.creeps[name];
         if(creep.memory.role == 'harvester') {
