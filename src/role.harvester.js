d5bd336602699e20f69ba23781748da5e81a59dc
Jo Jolivet <joj@avi.com>
2025-05-05 04:27:36 -0400

    Move back to nearestest spawn.

--- src/role.harvester.js
+++ src/role.harvester.js
@@ -14,7 +14,7 @@ var roleHarvester = {
                         return (structure.structureType == STRUCTURE_EXTENSION ||
                                 structure.structureType == STRUCTURE_SPAWN ||
                                 structure.structureType == STRUCTURE_TOWER) && 
-                                structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
+                                (structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0 || structure.structureType == STRUCTURE_SPAWN);
                     }
             });
             if(targets.length > 0) {
